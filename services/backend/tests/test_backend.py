8b776ce6f80a58943262e3a0a73a95fc01ef3bf5
Daniel Lipniacki <daniel.lipniacki@protonmail.com>
2021-11-18 13:28:07 +0100

    projectInit

---
+++ services/backend/tests/test_backend.py
@@ -0,0 +1,13 @@
+from src import __version__, app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
