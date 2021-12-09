980448fefa0584b0082efc5e0f8f534970d1ee04
noFrostoo <daniel.lipniacki@protonmail.com>
2021-12-09 20:46:47 +0100

    add vuex and axios

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = 'http://'+ window.location.hostname +':5000/';  // the FastAPI backend
+
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
