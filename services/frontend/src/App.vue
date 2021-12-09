71dd2eb4e8604f297987590db14104fab607ab77
noFrostoo <daniel.lipniacki@protonmail.com>
2021-12-09 20:46:47 +0100

    add vuex and axios

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -35,4 +35,25 @@ export default {
   flex-flow: column;
   height: 100%;
 }
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
