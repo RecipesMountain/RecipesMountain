732fed4c1ee9d540f5e59927d62b5febecbf9cfc
Dumonoo <domino200@vp.pl>
2021-12-13 14:51:00 -0800

    Added basic register form without connection with backend

--- services/frontend/src/views/UserViews/Register.vue
+++ services/frontend/src/views/UserViews/Register.vue
@@ -1,1 +1,17 @@
-// register page
+<template>
+  <div>
+      <RegisterForm/>
+  </div>
+</template>
+
+<script>
+import RegisterForm from '@/components/user/RegisterForm.vue'
+export default {
+  components: { RegisterForm },
+
+}
+</script>
+
+<style>
+
+</style>
