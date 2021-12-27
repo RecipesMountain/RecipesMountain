b983cb5864cd29b242d828cc3ca549865859734c
noFrostoo <daniel.lipniacki@protonmail.com>
2021-12-27 23:18:07 +0100

    fix linter

--- services/backend/app/core/tasks.py
+++ services/backend/app/core/tasks.py
@@ -6,8 +6,8 @@ from app import crud
 from app.models.recipe import Recipe
 from fastapi import Depends
 
+
 @app.on_event("startup")
 @repeat_every(seconds=60 * 60 * 8)
-def recalculatePopularity(db = Depends(deps.get_db) ) -> None:
+def recalculatePopularity(db=Depends(deps.get_db)) -> None:
     recpies = crud.recipe.get_all(db)
-
