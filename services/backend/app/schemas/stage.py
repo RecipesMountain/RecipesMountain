d37ae3f09aaa4df0203f2d6045ce105c5f84bef4
Dumonoo <domino200@vp.pl>
2022-01-03 06:37:11 -0800

    Deleted comments in stages schema

--- services/backend/app/schemas/stage.py
+++ services/backend/app/schemas/stage.py
@@ -5,40 +5,6 @@ from uuid import UUID
 
 from .product import ProductWithAmount, Product
 
-#! name optional. content no ?
-# class StageBase(BaseModel):
-#     name: Optional[str]
-#     content: Optional[str]
-
-#     class Config:
-#         orm_mode = True
-
-
-# class StageWithIngredients(StageBase):
-#     products: List[ProductWithAmount]
-
-# class StageCreate(StageWithIngredients):
-#     name: str
-#     content: Optional[str]
-
-# class StageUpdate(StageBase):
-#     pass
-
-
-# class StageWithProduct:
-#     pass
-
-
-# class Stage(StageCreate):
-#     pass
-
-
-# class StageInDBBase(Stage):
-#     id: Optional[UUID] = None
-
-#     class Config:
-#         orm_mode = True
-
 class StageBase(BaseModel):
     name: Optional[str]
     content: Optional[str]
@@ -68,29 +34,3 @@ class StageInDB(StageWithIngredients):
     class Config:
         orm_mode = True
     
-
-
-
-# class StageTest(StageBase):
-#     name: str
-#     content: Optional[str]
-
-
-
-# # class Stage(StageInDB):
-# #     pass
-
-# class SBase(BaseModel):
-#     name: Optional[str]
-#     content: Optional[str]
-
-#     class Config:
-#         orm_mode = True
-
-# class CBase(SBase):
-#     name: str
-#     content: str
-#     products: List[ProductWithAmount]
-
-
-
