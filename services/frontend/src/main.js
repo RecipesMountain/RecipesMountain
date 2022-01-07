92a3e26f7faaae00eb5e59aaf6daad5ec49dc3ec
noFrostoo <daniel.lipniacki@protonmail.com>
2022-01-07 19:29:38 +0100

    fix

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import axios from 'axios';
 
 Vue.config.productionTip = false
 
