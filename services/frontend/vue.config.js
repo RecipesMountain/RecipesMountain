54df89625b0926186d0c4dbebf957ef5610198b7
Filip Lew <46330610+lorlen@users.noreply.github.com>
2022-01-08 10:26:47 +0100

    add API proxy for dev profile (#27)

--- services/frontend/vue.config.js
+++ services/frontend/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://backend:5000',
+      },
+    },
+  },
 }
